02c1ba3a4f46fc9b637ad4a3b4fa95c9e79af712
MINSK\Aliaksandr_Khatsko <aliaksandr.kh@gmail.com>
2016-11-18 19:11:05 +0300

    First commit Pre-task 1.1

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class CalculatorApp
+    {
+
+     
+        static void Main(string[] args)
+        {
+            Boolean calcNeverStop = false;
+            do
+            {
+                Double firstNum = 0;
+                Boolean correctNumber1 = false;
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Please, enter first number");
+                        String firstNumberStr = Console.ReadLine();
+                        firstNum = Double.Parse(firstNumberStr);
+                        correctNumber1 = true;
+                    }
+                    catch (System.FormatException e)
+                    {
+                        Console.WriteLine("Wrong characters, please try again");
+                    }
+                } while (correctNumber1 == false);
+
+
+                String op;
+                Boolean correctOperator = false;
+                Console.WriteLine("Please, choose an operator (+,-,*,/)");
+                op = Console.ReadLine();
+                do
+                {
+                    if (String.Equals(op, "+"))
+                    {
+                        correctOperator = true;
+                    }
+
+                    else if (String.Equals(op, "-"))
+                    {
+                        correctOperator = true;
+                    }
+
+                    else if (String.Equals(op, "/"))
+                    {
+                        correctOperator = true;
+                    }
+
+                    else if (String.Equals(op, "*"))
+                    {
+                        correctOperator = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong operator, choose an operator (+,-,*,/)");
+                        op = Console.ReadLine();
+                    }
+                } while (correctOperator == false);
+
+
+                Double secondNum = 0;
+                Boolean correctNumber2 = false;
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Please, enter second number");
+                        String secondNumberStr = Console.ReadLine();
+                        secondNum = Double.Parse(secondNumberStr);
+                        correctNumber2 = true;
+                    }
+                    catch (System.FormatException ex)
+                    {
+                        Console.WriteLine("Wrong characters, please try again");
+                    }
+
+                } while (correctNumber2 == false);
+
+
+
+                Double result = 0;
+                switch (op)
+                {
+                    case "+":
+                        result = MathOperations.add(firstNum, secondNum);
+                        Console.WriteLine("Result = " + result + "  (Press Enter to continue)");
+                        break;
+                    case "-":
+                        result = MathOperations.substract(firstNum, secondNum);
+                        Console.WriteLine("Result = " + result + "  (Press Enter to continue)");
+                        break;
+                    case "*":
+                        result = MathOperations.multiply(firstNum, secondNum);
+                        Console.WriteLine("Result = " + result + "  (Press Enter to continue)");
+                        break;
+                    case "/":
+                        if (secondNum != 0)
+                        {
+                            result = MathOperations.divide(firstNum, secondNum);
+                            Console.WriteLine("Result = " + result + "  (Press Enter to continue)");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Division of {0} by zero is impossible. Please, press Enter and try again", firstNum);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                
+                Console.ReadLine();
+
+            } while (calcNeverStop == false);
+            
+        }
+                  
+
+    }
+        
+
+}
---
+++ CalculatorApp/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class MathOperations
+    {
+        public static double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
